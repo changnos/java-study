f73d3b7657d8f22afcb57919a738f198f8b1ab12
Changjin Lee <changjini32@gmail.com>
2024-11-28 12:51:19 +0900

    Echo Client #1

--- network/src/main/java/test/TCPServer.java
+++ network/src/main/java/test/TCPServer.java
@@ -6,6 +6,7 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
 
 public class TCPServer {
 
@@ -51,8 +52,10 @@ public class TCPServer {
 					os.write(data.getBytes("UTF-8"));
 				}
 
+			} catch (SocketException e) {
+				System.out.println("[server] Socket Exception: " + e);
 			} catch (IOException e) {
-				System.out.println("error: " + e);
+				System.out.println("[server] error: " + e);
 			} finally {
 				try {
 					if (socket != null && !socket.isClosed()) {
