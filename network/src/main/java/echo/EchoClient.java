ced374dd488364d1f6114d0ce25a0b5184eb6736
Changjin Lee <changjini32@gmail.com>
2024-11-28 17:08:57 +0900

    httpd #1

--- network/src/main/java/echo/EchoClient.java
+++ network/src/main/java/echo/EchoClient.java
@@ -1,46 +1,55 @@
 package echo;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class EchoClient {
 	private static final String SERVER_IP = "127.0.0.1";
 
 	public static void main(String[] args) {
+		Scanner scanner = null;
 		Socket socket = null;
 
 		try {
-			// 1. 소켓생성
+			scanner = new Scanner(System.in);
 			socket = new Socket();
 
-			// 2. 서버연결
 			socket.connect(new InetSocketAddress(SERVER_IP, EchoServer.PORT));
 
-			// 3. IO Stream 받아오기
-			InputStream is = socket.getInputStream();
-			OutputStream os = socket.getOutputStream();
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+			BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
 
-			// 4. 쓰기
-			String data = "Hello Wolrd";
-			os.write(data.getBytes("UTF-8"));
+			while (true) {
+				System.out.println(">>");
+				String line = scanner.nextLine();
 
-			// 5. 읽기
-			byte[] buffer = new byte[256];
-			int readByteCount = is.read(buffer);
-			if (readByteCount == -1) {
-				log("closed by server");
-				return;
+				if ("exit".equals(line)) {
+					break;
+				}
+
+				pw.println(line);
+				
+				String data = br.readLine();
+				if (data == null) {
+					log("closed by server");
+					break;
+				}
+				System.out.println("<<" + data);
 			}
-
-			data = new String(buffer, 0, readByteCount, "UTF-8");
-			log("received: " + data);
 		} catch (IOException e) {
 			log("error: " + e);
 		} finally {
 			try {
+				if (scanner != null) {
+					scanner.close();
+				}
+
 				if (socket != null && !socket.isClosed()) {
 					socket.close();
 				}
@@ -51,7 +60,7 @@ public class EchoClient {
 	}
 
 	public static void log(String message) {
-		log("[Echo Client] " + message);
+		System.out.println("[Echo Client] " + message);
 	}
 
 }
--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,14 +1,9 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
 	public static final int PORT = 60000;
@@ -20,41 +15,9 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", PORT));
 			log("starts...[port:" + PORT + "]");
 
-			Socket socket = serverSocket.accept();
-
-			try {
-				InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-				String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-				int remotePort = inetRemoteSocketAddress.getPort();
-
-				log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
-
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-
-				while (true) {
-					String data = br.readLine();
-					if (data == null) {
-						log("closed by client");
-						break;
-					}
-					log("received: " + data);
-
-					pw.println(data);
-				}
-
-			} catch (SocketException e) {
-				log("Socket Exception: " + e);
-			} catch (IOException e) {
-				log("error: " + e);
-			} finally {
-				try {
-					if (socket != null && !socket.isClosed()) {
-						socket.close();
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
+			while (true) {
+				Socket socket = serverSocket.accept();
+				new EchoRequestHandler(socket).start();
 			}
 		} catch (IOException e) {
 			log("error: " + e);
